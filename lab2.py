212d696647364dd70ef1de7170740cb8906cf545
joacar <j.carselind@gmail.com>
2011-09-27 17:59:10 +0200

    Now the test case works. Could be simplified and made more elegant. Missing is the PLOT to visualize.

--- lab2.py
+++ lab2.py
@@ -2,7 +2,7 @@
 from cvxopt.base import matrix
 from cvxopt.solvers import qp
 
-import random, math, numpy, pprint, matplotlib
+import random, math, numpy, pprint, matplotlib, test
 
 TRESHOLD = math.pow(10, -9)
 
@@ -35,8 +35,10 @@ def solve_qp(P):
 		return I
 	
 	N = len(P)
-	q = numpy.ones(N)
-	h = numpy.zeros(N)
+	q = numpy.ones( ( N, 1 ) )
+	h = numpy.zeros( ( N, 1 ) )
+	#q = numpy.ones((N, 1))*-1
+	#h = numpy.zeros((N, 1))
 	numpy.transpose(h)
 	G = -identity(N)
 	print "N = ", len(P)
@@ -54,13 +56,13 @@ def solve_qp(P):
 	alpha = list(r['x'])
 	return alpha
 
-def indicator_function(new_dp, alpha):
+def indicator_function(new_dp, alpha_list):
 	"INPUT the data point to be classified and non-zero alpha values with corresponding data point"
 	"RETURN true iff correctly classified"
 	def evaluate(a, i, x_, x):
 		return a*i*linear_kernel(x_, x)
 	value = 0;
-	for (a, dp) in alpha:
+	for (a, dp) in alpha_list:
 		indicator = dp[2]
 		point = (dp[0], dp[1])
 		value += evaluate(a, indicator, new_dp, point)
@@ -107,7 +109,8 @@ def printMatrix(matrix):
 data = generate_data()
 
 # Build the matrix P according to P(i,j) = t(i)*t(j)*K(x(i), x(j)) where K is kernel function
-P = buildP(data)
+test_data = test.test1()
+P = buildP(test_data)
 
 # Solve the quadractic problem
 alpha = solve_qp(P)
@@ -125,11 +128,21 @@ print "TRESHOLD = ", TRESHOLD
 print "Positive alpha\n", str(positive_alpha)
 
 new_dp = (random.normalvariate(1, 0.5),  random.normalvariate(2, 0.25))
+verification = test.verify1()
 # Classify a new data point using the indicator function
 value = indicator_function(new_dp, positive_alpha)
 
-print value
+pos_list = []
+for p in verification[0]:
+	pos_list += [(indicator_function(p, positive_alpha), p[0], p[1])]
+#pos_list = map(lambda x: indicator_function(x, positive_alpha), verification[0])
+#neg_list = map(lambda x: indicator_function(x, positive_alpha), verification[1])
 
+cor = filter(lambda x: x[0] > 0, pos_list)
+not_cor = filter(lambda x: x[0] <= 0, pos_list)
+print "\nCorrect classified examples:\n", str(cor)
+
+print "\nIn-correct classified examples:\n", str(not_cor)
 #print data
 #pp = pprint.PrettyPrinter(depth=2)
 #pp.pprint(map(repr,buildP(data)))
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import random, lab2
+import random
 
 # Test when all positive/negative are above/below x-axis
-t1_size = 10;
-def test_above_below_x_axis():
+# test_above_below_x_axis
+t1_size = 10
+def test1():
 	pos_pos_x = [ (random.normalvariate(5, 4), random.normalvariate(4, 2), 1) for x in range(t1_size)]
 	pos_neg_x = [ (random.normalvariate(-5, 4), random.normalvariate(4, 2), 1) for x in range(t1_size)]
 	above_x = pos_pos_x + pos_neg_x
@@ -16,9 +17,14 @@ def test_above_below_x_axis():
 	data = above_x + below_x
 	random.shuffle(data)
 
-v1_size = 20;
-def verify_above_below_x_axis():
+	return data
+
+# verify__above_below_x_axis
+v1_size = 20
+def verify1():
 	# y-value is set to normal(10, 0) meaning ALL will be above test points using normal(4,2)
-	pos = [ (random.normalvariate(9, 10), random.normalvariate(10, 0), 1) for x in range(v1_size)]
+	pos = [ (random.normalvariate(9, 10), random.normalvariate(10, 0)) for x in range(v1_size)]
 	# y-value is set to normal(-10, 0) meaning ALL will be below negative test points using normal(-4,2)
-	neg = [ (random.normalvariate(-5, 4), random.normalvariate(-10, 0), -1) for x in range(t1_size)]
+	neg = [ (random.normalvariate(-5, 4), random.normalvariate(-10, 0)) for x in range(t1_size)]
+
+	return (pos, neg)
