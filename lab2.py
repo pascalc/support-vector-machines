770944ff4e7e7ea2ea431a95cb4ccccba4e356e6
joacar <j.carselind@gmail.com>
2011-09-29 08:54:19 +0200

    Built a test class that performs the test. Changed made to lab2.py to match this.

--- lab2.py
+++ lab2.py
@@ -2,33 +2,13 @@
 from cvxopt.base import matrix
 from cvxopt.solvers import qp
 
-import random, math, numpy, pprint, matplotlib, pylab
-import test, data
+import math, numpy
+import data
 
 THRESHOLD = math.pow(10, -5)
+DEBUG = 1
 
-def plot_points(classA, classB):
-	pylab.hold(True)
-	pylab.plot([p[0] for p in classA],
-		[p[1]for p in classA],
-	'bo')
-	pylab.plot([p[0] for p in classB],
-		[p[1]for p in classB],
-	'ro')
-	pylab.show()
-
-def plot_decision_boundary(x, alpha_list):
-	x_range = numpy.arange(-4, 4, 0.05)
-	y_range = numpy.arange(-4, 4, 0.05)
-	grid = matrix([ [indicator_function(x, y)
-		for y in y_range]
-		for x in x_range])
-	pylab.contour(x_range, y_range, grid,
-		(-1.0, 0.0, 1.0),
-		colors=('red',' black', 'blue'),
-		leninewidths=(1,3,1))
-
-def solve_qp(P):
+def solveQP(P):
 	"INPUT is a matrix P with dim NxN"
 	"RETURN alpha that max/min? shiiiiit"
 	def identity(n):
@@ -41,57 +21,42 @@ def solve_qp(P):
 	N = len(P)
 	q = numpy.ones((N,1))*(-1)
 	h = numpy.zeros((N,1))
-	#q = numpy.ones((N, 1))*-1
-	#h = numpy.zeros((N, 1))
 	numpy.transpose(h)
 	G = -identity(N)
-	# print "N = ", len(P)
-	# print "matrix(q).size = ", matrix(q).size
-	# print "matrix(h).size = ", matrix(h).size
-	# print "matrix(G).size = ", matrix(G).size
-	# print "matrix(P).size = ", matrix(P).size
-
-	# print matrix(q)
-	# print matrix(h)
-	# print matrix(G)
-	# print matrix(P)
-
+	
 	r = qp(matrix(P), matrix(q), matrix(G), matrix(h))
 	alpha = list(r['x'])
 	return alpha
 
-def indicator_function(new_dp, alpha_list, kernel_function):
+def indicatorFunction(new_dp, alpha_list, kernel_function):
 	"INPUT the data point to be classified and non-zero alpha values with corresponding data point"
-	"RETURN true iff correctly classified"
-	# def evaluate(a, i, x_, x):
-	# 	return a*i*kernel_function(x_, x)
+	"RETURN indicator value, positive if correctly classified"
 	value = 0;
 	for (a, dp) in alpha_list:
 		indicator = dp[2]
 		point = (dp[0], dp[1])
-		#value += evaluate(a, indicator, new_dp, point)
 		value += a*indicator*kernel_function(new_dp,point)
 	
 	return value
 
-def linear_kernel(x,y):
+def linearKernel(x,y):
 	res = matrix(x).trans() * matrix(y) + 1
 	return res[0]
 
-def radial_basis_kernel(x, y, sigma):
+def radialBasisKernel(x, y, sigma):
 	def sub_vector(x, y):
-		#z = ((x[0] - y[0]), (x[1] - y[1])
 		z = matrix(x) - matrix(y)
-
 		return z[0]
 	
 	enumerator = math.pow(sub_vector(x, y))
-	denumerator = math.pow(sigma, 2)
+	denominator = math.pow(sigma, 2)
 	K = math.exp(enumerator / denumerator)
 
 	return K
 
 def buildP(raw_data, kernel_function):
+	"INPUT data, kernel function K"
+	"RETURN matrix P(i,j) = t(i)*t(j)*K(x(i), x(j))"
 	P = []
 	data = map(lambda r: (r[0],r[1]), raw_data)
 	indicators = map(lambda r: r[2], raw_data)
@@ -103,56 +68,41 @@ def buildP(raw_data, kernel_function):
 			row += [indicators[i]*indicators[j]*kernel_function(x,y)]
 		P += [row]
 	return P
+
+def debug(message):
+	if DEBUG == 0:
+		return
+	print message
+
+def main(test):
+	data = test.getData()
+	classify = test.classify
+
+	P = buildP(data, linearKernel)
+
+	# Solve the quadractic problem
+	alpha = solveQP(P)
+	debug("\nalpha = " + str(alpha))
+
+	# Map the alpha values to their corresponding data points
+	data_alpha = zip(alpha, data) 
+	debug("\nAlpha values with corresponding points\n" + str(data_alpha))
+
+	# Sort out the positive using THRESHOLD
+	positive_alpha = filter(lambda x: x[0] >= THRESHOLD, data_alpha)
+	debug("\nPositive alpha\n" + str(positive_alpha))
+
+
+	# Classify a new data point using the indicator function
+	classify_list = []
+	for p in classify:
+		classify_list += [(indicatorFunction(p, positive_alpha, linearKernel), p[0], p[1])]
+
+	correct_classified = filter(lambda x: x[0] > 0, classify_list)
+	debug("\nCorrect classified examples: " + str(correct_classified))
 	
-def printMatrix(matrix):
-	for i in range(len(data)):
-		for j in range(len(data)):
-			print matrix[i][j]
-		print
+	incorrect_classified = filter(lambda x: x[0] <= 0, classify_list)
+	debug("\nIncorrect classified examples: " + str(incorrect_classified))
 
-classA = data.classA
-classB = data.classB
-
-#plot_points(classA, classB)
-plot_points(data.above_x, data.below_x)
-
-# Generate the test data
-data = classA + classB
-random.shuffle(data)
-
-# Build the matrix P according to P(i,j) = t(i)*t(j)*K(x(i), x(j)) where K is kernel function
-test_data = test.test1()
-P = buildP(test_data, linear_kernel)
-
-# Solve the quadractic problem
-alpha = solve_qp(P)
-
-print "\nalpha = ", str(alpha)
-
-# Map the alpha values to their corresponding data points
-data_alpha = zip(alpha, data) 
-print "\nAlpha values with corresponding points\n", str(data_alpha)
-
-# Sort out the positive using THRESHOLD
-positive_alpha = filter(lambda x: x[0] >= THRESHOLD, data_alpha)
-print "\nPositive alpha\n", str(positive_alpha)
-
-new_dp = (random.normalvariate(1, 0.5),  random.normalvariate(2, 0.25))
-verification = test.verify1()
-# Classify a new data point using the indicator function
-value = indicator_function(new_dp, positive_alpha, linear_kernel)
-
-pos_list = []
-for p in verification[0]:
-	pos_list += [(indicator_function(p, positive_alpha), p[0], p[1])]
-#pos_list = map(lambda x: indicator_function(x, positive_alpha), verification[0])
-#neg_list = map(lambda x: indicator_function(x, positive_alpha), verification[1])
-
-cor = filter(lambda x: x[0] > 0, pos_list)
-not_cor = filter(lambda x: x[0] <= 0, pos_list)
-print "\nCorrect classified examples:\n", str(cor)
-
-print "\nIn-correct classified examples:\n", str(not_cor)
-#print data
-#pp = pprint.PrettyPrinter(depth=2)
-#pp.pprint(map(repr,buildP(data)))
+	test.plotData()
+	#test.plotDecisionBoundary()
--- test.py
+++ test.py
@@ -1,30 +1,69 @@
 #!/usr/bin/env python
 
-import random
+# Test class to simply testing and plot charts
+# Authors: Joakim Carselind and Pascal Chatterjee
+# Version: 2011-09-29
+
+import random, pylab, matplotlib
+import lab2 as svm
+
+class TestClass:
+	"INPUT data tripple containaing, in order: positive data, negative data, classification points"
+	"RETURN #N/A"
+	def __init__(self, data):
+		self.positive = data[0]
+		self.negative = data[1]
+		self.classify = data[2]
+
+	def getData(self):
+		data = self.positive + self.negative
+		random.shuffle(data)
+		return data
+	
+	def plotData(self):
+		"Plots the positive and negative data points"
+		pylab.hold(True)
+		pylab.plot([p[0] for p in self.positive],
+			[p[1]for p in self.positive],
+		'bo')
+		pylab.plot([p[0] for p in self.negative],
+			[p[1]for p in self.negative],
+		'ro')
+		pylab.show()
+	
+	def plotDecisionBoundary(x, y, indicator):
+		x_range = numpy.arange(-4, 4, 0.05)
+		y_range = numpy.arange(-4, 4, 0.05)
+		grid = matrix([ [indicator(x, y)
+			for y in y_range]
+			for x in x_range])
+		pylab.contour(x_range, y_range, grid,
+			(-1.0, 0.0, 1.0),
+			colors=('red',' black', 'blue'),
+			leninewidths=(1,3,1))
 
 # Test when all positive/negative are above/below x-axis
-# test_above_below_x_axis
-t1_size = 10
-def test1():
-	pos_pos_x = [ (random.normalvariate(5, 4), random.normalvariate(4, 2), 1) for x in range(t1_size)]
-	pos_neg_x = [ (random.normalvariate(-5, 4), random.normalvariate(4, 2), 1) for x in range(t1_size)]
-	above_x = pos_pos_x + pos_neg_x
+def test1_linear():
+	print "\n\t***** test1_linear *****\n"
+	def classify():
+		"classification points"
+		v1_size = 20
+		# y-value is set to normal(10, 0) meaning ALL will be above test points using normal(4,2)
+		pos = [ (random.normalvariate(9, 10), random.normalvariate(10, 0)) for x in range(v1_size)]
+		# y-value is set to normal(-10, 0) meaning ALL will be below negative test points using normal(-4,2)
+		neg = [ (random.normalvariate(-5, 4), random.normalvariate(-10, 0)) for x in range(t1_size)]
+		return pos + neg
 
-	neg_pos_x = [ (random.normalvariate(5, 4), random.normalvariate(-4, 2), -1) for x in range(t1_size)]
-	neg_neg_x = [ (random.normalvariate(-5, 4), random.normalvariate(-4, 2), -1) for x in range(t1_size)]
-	below_x = neg_neg_x + neg_pos_x
+	t1_size = 10
+	first_qudrant = [ (random.normalvariate(5, 4), random.normalvariate(4, 2), 1) for x in range(t1_size)]
+	second_quadrant = [ (random.normalvariate(-5, 4), random.normalvariate(4, 2), 1) for x in range(t1_size)]
+	positive = first_qudrant + second_quadrant
 
-	data = above_x + below_x
-	random.shuffle(data)
+	third_qudrant = [ (random.normalvariate(5, 4), random.normalvariate(-4, 2), -1) for x in range(t1_size)]
+	fourth_qudrant = [ (random.normalvariate(-5, 4), random.normalvariate(-4, 2), -1) for x in range(t1_size)]
+	negative = third_qudrant + fourth_qudrant
 
-	return data
+	return (positive, negative, classify())
 
-# verify__above_below_x_axis
-v1_size = 20
-def verify1():
-	# y-value is set to normal(10, 0) meaning ALL will be above test points using normal(4,2)
-	pos = [ (random.normalvariate(9, 10), random.normalvariate(10, 0)) for x in range(v1_size)]
-	# y-value is set to normal(-10, 0) meaning ALL will be below negative test points using normal(-4,2)
-	neg = [ (random.normalvariate(-5, 4), random.normalvariate(-10, 0)) for x in range(t1_size)]
-
-	return (pos, neg)
+data = test1_linear()
+svm.main(TestClass(data))
